5d5f46ce2fd14388ed1d5d9457e6db54c1e95f54
Jean-Daniel Bonnetot <jean-daniel.bonnetot@corp.ovh.com>
2017-10-12 21:04:28 +0000

    Add some hole in app module

--- terraform-modules/app/.main.tf
+++ terraform-modules/app/.main.tf
@@ -33,6 +33,14 @@ resource "tls_private_key" "shared_ssh_key" {
   algorithm = "RSA"
 }
 
+data "template_file" "frontend_userdata" {
+  template = "${file("${path.module}/frontweb.yaml")}"
+
+  vars {
+    ssh_shared_priv_key = "${indent(7, tls_private_key.shared_ssh_key.private_key_pem)}"
+  }
+}
+
 data "template_file" "lb_userdata" {
   template = "${file("${path.module}/loadbalancer.yaml")}"
 
@@ -53,6 +61,11 @@ resource "openstack_compute_instance_v2" "backend" {
     access_network = true
   }
 
+  network {
+    name        = "${openstack_networking_network_v2.privatenet-test.name}"
+    fixed_ip_v4 = "10.1.254.254"
+  }
+
   user_data = "${file("${path.module}/backend.yaml")}"
 }
 
@@ -68,6 +81,7 @@ resource "openstack_compute_instance_v2" "loadbalancer" {
     access_network = true
   }
 
+  user_data = "${data.template_file.lb_userdata.rendered}"
 }
 
 resource "openstack_compute_instance_v2" "frontweb" {
